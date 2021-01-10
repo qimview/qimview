3081d8b69d73280cc274f0c001464dafe92c1cf5
karl krissian <karl.krissian@gmail.com>
2021-01-10 14:33:21 +0100

    C++ code with binding to process scalar and RGB images

--- CppBind/wrap_numpy.cpp
+++ CppBind/wrap_numpy.cpp
@@ -1,6 +1,12 @@
 // cppimport this line allows to use import directly
 <%
-cfg['compiler_args'] = ['-std=c++11', '-stdlib=libc++', '-mmacosx-version-min=10.7', '/openmp']
+import sys
+if sys.platform == 'linux':
+    cfg['compiler_args'] = ['-std=c++11', '-fopenmp', '-O3']
+    cfg['linker_args'] = ['-fopenmp']
+else:
+    cfg['compiler_args'] = ['/openmp']
+# cfg['compiler_args'] = ['-std=c++11', '-stdlib=libc++', '-mmacosx-version-min=10.7', '/openmp', '/Ox']
 setup_pybind11(cfg)
 %>
 
@@ -17,18 +23,8 @@ namespace py = pybind11;
 
 using namespace pybind11::literals;
 
-template <class data_type>
-void apply_filters(py::array_t<data_type> x)
-{
-    auto r = x.mutable_unchecked<3>(); // Will throw if ndim != 3 or flags.writeable is false
-	#pragma omp parallel for
-    for (py::ssize_t i = 0; i < r.shape(0); i++)
-        for (py::ssize_t j = 0; j < r.shape(1); j++)
-            for (py::ssize_t k = 0; k < r.shape(2); k++)
-                r(i, j, k) += static_cast<data_type>(2);
-}
-
-
+#define CH_RGB 1
+#define CH_BGR 2
 #define CH_RGGB 4 // phase 0, bayer 2
 #define CH_GRBG 5 // phase 1, bayer 3 (Boilers)
 #define CH_GBRG 6 // phase 2, bayer 0
@@ -37,8 +33,100 @@ void apply_filters(py::array_t<data_type> x)
 #define G 1
 #define B 2
 
+
+// convert input scalar image to output RGB image applying image filters
 template <class input_type, class output_type>
-bool apply_filters_RAW(
+bool apply_filters_scalar(
+        py::array_t<input_type> in,
+        py::array_t<output_type> out,
+        float black_level,
+        float white_level,
+        input_type max_value, // maximal value based on image precision
+        output_type max_type,  // maximal value based on image type (uint8, etc...)
+        float gamma
+)
+{
+    auto input  = in.template unchecked<2>(); // Will throw if ndim != 2
+    auto output = out.template mutable_unchecked<3>(); // Will throw if ndim != 3 or flags.writeable is false
+
+    // input has limited values possibilities from 0 to max_value, we can precompute all the results
+    // it will save time especially if gamma is used
+//    outputtype output_lut[4096*3];
+    // lookup table is interesting if the number of input values is clearly inferior to the number of pixels
+
+
+    if (max_value < 2*input.size())
+    {
+        auto output_lut = new output_type[(max_value+1)];
+        auto output_lut_pos = output_lut;
+
+        for(int v=0; v<=max_value; v++, output_lut_pos++) {
+            float y;
+            // normalize to 1
+            y = float(v)/max_value;
+            // black level
+            y  = (y<black_level)?0:(y-black_level);
+            // rescale to white level as saturation level
+            y  /= (white_level-black_level);
+            if (gamma!=1) {
+                float p = 1.0f/gamma;
+                // apply gamma
+                y  = powf(y, p);
+            }
+            // for the moment put result in first three components
+            *output_lut_pos = static_cast<output_type>(std::min(1.f,y)*255.f);
+        }
+
+        #pragma omp parallel for
+        for (py::ssize_t i = 0; i < input.shape(0); i++)
+        {
+            for (py::ssize_t j = 0; j < input.shape(1); j++)
+            {
+                input_type val   = std::min(max_value,(input_type)input(i, j));
+                output_type val_out = output_lut[val];
+                auto output_ptr = &output(i, j, 0);
+                *output_ptr++ = val_out;
+                *output_ptr++ = val_out;
+                *output_ptr   = val_out;
+           }
+       }
+
+       delete [] output_lut;
+    }
+    else {
+        #pragma omp parallel for
+        for (py::ssize_t i = 0; i < input.shape(0); i++)
+        {
+            for (py::ssize_t j = 0; j < input.shape(1); j++)
+            {
+                input_type v   = std::min(max_value,(input_type)input(i, j));
+                float y;
+                // normalize to 1
+                y = float(v)/max_value;
+                // black level
+                y  = (y<black_level)?0:(y-black_level);
+                // rescale to white level as saturation level
+                y  /= (white_level-black_level);
+                if (gamma!=1) {
+                    float p = 1.0f/gamma;
+                    // apply gamma
+                    y  = powf(y, p);
+                }
+                // for the moment put result in first three components
+                output_type val_out = static_cast<output_type>(std::min(1.f,y)*255.f);
+                auto output_ptr = &output(i, j, 0);
+                *output_ptr++ = val_out;
+                *output_ptr++ = val_out;
+                *output_ptr   = val_out;
+           }
+       }
+    }
+    return true;
+}
+
+
+template <class input_type, class output_type>
+bool apply_filters(
         py::array_t<input_type> in,
         py::array_t<output_type> out,
         int channels, // channel representation
@@ -51,11 +139,12 @@ bool apply_filters_RAW(
         float gamma
 )
 {
-    auto input  = in.unchecked<3>(); // Will throw if ndim != 3
-    auto output = out.mutable_unchecked<3>(); // Will throw if ndim != 3 or flags.writeable is false
+    auto input  = in.template unchecked<3>(); // Will throw if ndim != 3
+    auto output = out.template mutable_unchecked<3>(); // Will throw if ndim != 3 or flags.writeable is false
 
 	// assume shape(2) is 4
-	if (input.shape(2) != 4)
+	const int nb_channels = input.shape(2);
+	if ((nb_channels != 4) && (nb_channels!=3))
 	{
 	    //std::cout << "Number of channels is not 4 ", r.shape(2) << std::endl;
 	    return false;
@@ -97,12 +186,14 @@ bool apply_filters_RAW(
     }
 
     // transform bayer input to RGB
-    int r,gr,gb,b;
+    int r,gr,gb,b, g = -1;
     switch (channels) {
-    case 4:   r = 0; gr = 1; gb = 2; b = 3;  break; // CH_RGGB = 4 phase 0, bayer 2
-    case 5:   r = 1; gr = 0; gb = 3; b = 2;  break; // CH_GRBG = 5 phase 1, bayer 3 (Boilers)
-    case 6:   r = 2; gr = 3; gb = 0; b = 1;  break; // CH_GBRG = 6 phase 2, bayer 0
-    case 7:   r = 3; gr = 2; gb = 1; b = 0;  break; // CH_BGGR = 7 phase 3, bayer 1 (Coconuts)
+    case CH_RGB:   r = 0; g = 1; b = 2;  break;
+    case CH_BGR:   r = 2; g = 1; b = 0;  break;
+    case CH_RGGB:  r = 0; gr = 1; gb = 2; b = 3;  break; // CH_RGGB = 4 phase 0, bayer 2
+    case CH_GRBG:  r = 1; gr = 0; gb = 3; b = 2;  break; // CH_GRBG = 5 phase 1, bayer 3 (Boilers)
+    case CH_GBRG:  r = 2; gr = 3; gb = 0; b = 1;  break; // CH_GBRG = 6 phase 2, bayer 0
+    case CH_BGGR:  r = 3; gr = 2; gb = 1; b = 0;  break; // CH_BGGR = 7 phase 3, bayer 1 (Coconuts)
     default:  r = 0; gr = 1; gb = 2; b = 3;  break; // this should not happen
     }
 
@@ -117,8 +208,12 @@ bool apply_filters_RAW(
             // try to speed-up input data access
             auto input_ptr = &input(i, j, 0);
             input_type red   = std::min(max_value,(input_type)input_ptr[r]);
-            input_type green = std::min(max_value,(input_type) ((input_ptr[gr]+input_ptr[gb]+1)>>1));
             input_type blue  = std::min(max_value,(input_type)(input_ptr[b]));
+            input_type green;
+            if (g==-1) // raw data
+                green = std::min(max_value,(input_type) ((input_ptr[gr]+input_ptr[gb]+1)>>1));
+            else // 3 channels RGB or BGR data
+                green  = std::min(max_value,(input_type)(input_ptr[g]));
 
             // for the moment put result in first three components
             auto output_ptr = &output(i, j, 0);
@@ -140,14 +235,51 @@ bool apply_filters_RAW(
  *
 */
 PYBIND11_MODULE(wrap_numpy, m) {
-    m.def("apply_filters_float", &apply_filters<float>, py::arg().noconvert());
-    m.def("apply_filters_uint16", &apply_filters<uint16_t>, py::arg().noconvert());
-    m.def("apply_filters_RAW", &apply_filters_RAW<uint16_t, uint8_t>,
+    m.def("apply_filters_u16_u8", &apply_filters<uint16_t, uint8_t>,
     py::arg().noconvert(),
     py::arg().noconvert(),
     py::arg(),
     py::arg(),
     py::arg(),
+    py::arg(),
+    py::arg(),
+    py::arg(),
+    py::arg(),
+    py::arg()
+     );
+    m.def("apply_filters_u8_u8", &apply_filters<uint8_t, uint8_t>,
+    py::arg().noconvert(),
+    py::arg().noconvert(),
+    py::arg(),
+    py::arg(),
+    py::arg(),
+    py::arg(),
+    py::arg(),
+    py::arg(),
+    py::arg(),
+    py::arg()
+     );
+    m.def("apply_filters_scalar_u8_u8", &apply_filters_scalar<uint8_t, uint8_t>,
+    py::arg().noconvert(),
+    py::arg().noconvert(),
+    py::arg(),
+    py::arg(),
+    py::arg(),
+    py::arg(),
+    py::arg()
+     );
+    m.def("apply_filters_scalar_u16_u8", &apply_filters_scalar<uint16_t, uint8_t>,
+    py::arg().noconvert(),
+    py::arg().noconvert(),
+    py::arg(),
+    py::arg(),
+    py::arg(),
+    py::arg(),
+    py::arg()
+     );
+    m.def("apply_filters_scalar_u32_u8", &apply_filters_scalar<uint32_t, uint8_t>,
+    py::arg().noconvert(),
+    py::arg().noconvert(),
     py::arg(),
     py::arg(),
     py::arg(),
