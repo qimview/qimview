86aa5f35e98ebdee26ccc2bef061a1d2399eb999
karl krissian <karl.krissian@gmail.com>
2021-01-10 14:47:25 +0100

    Add NumericParameterGui class, inherits from QSlider

--- image_viewers/ImageFilterParametersGui.py
+++ image_viewers/ImageFilterParametersGui.py
@@ -1,86 +1,106 @@
 from Qt import QtWidgets, QtCore, QtGui
 from image_viewers.ImageFilterParameters import ImageFilterParameters
+import types
+
+
+class NumericParameterGui(QtWidgets.QSlider):
+    """
+    For the moment, it can only be a slider with associated text
+    """
+    def __init__(self, name, param, callback, layout=None, parent_name=""):
+        QtWidgets.QSlider.__init__(self, QtCore.Qt.Horizontal)
+        self.name = name
+        self.param = param
+        self.callback = callback
+        self.event_recorder = None
+        self.parent_name = parent_name
+        self.widget_name = f"slider_{self.parent_name}_{self.name}"
+        if layout is not None:
+            self.create()
+            self.add_to_layout(layout)
+
+    def set_event_recorder(self, evtrec):
+        self.event_recorder = evtrec
+        if self.event_recorder is not None:
+            self.event_recorder.register_widget(id(self), self.widget_name)
+
+    def register_event_player(self, event_player):
+        event_player.register_widget(self.widget_name, self)
+
+    def create(self):
+        self.label = QtWidgets.QLabel("")
+        self.setRange(self.param.range[0], self.param.range[1])
+        self.setValue(self.param.value)
+        self.changed()
+        self.valueChanged.connect(lambda: self.changed(self.callback))
+
+    def add_to_layout(self, layout):
+        layout.addWidget(self.label)
+        layout.addWidget(self)
+
+    def reset(self):
+        self.setValue(self.param.default_value)
+
+    def changed(self, callback=None):
+        self.param.int = int(self.value())
+        self.label.setText(f"{self.name} {self.param.float:0.2f}")
+        if callback is not None:
+            callback()
+
+    def mouseDoubleClickEvent(self, evt):
+        self.reset()
+
+    def event(self, evt):
+        if self.event_recorder is not None:
+            if evt.spontaneous():
+                self.event_recorder.store_event(self, evt)
+        return QtWidgets.QSlider.event(self, evt)
 
 
 class ImageFilterParametersGui:
-    def __init__(self, parameters):
+    def __init__(self, parameters, name=""):
         """
         :param parameters: instance of ImageFilterParameters
         """
-        self.params = parameters
+        self.params    = parameters
+        self.bl_gui    = None
+        self.wl_gui    = None
+        self.gamma_gui = None
+        self.g_r_gui   = None
+        self.g_b_gui   = None
+        self.event_recorder = None
+        self.name           = name
+
+    def set_event_recorder(self, evtrec):
+        self.event_recorder = evtrec
 
     def add_blackpoint(self, layout, callback):
-        """
-        Black point adjustment
-        :param layout:
-        :param callback:
-        :return:
-        """
-        black_level = self.params.black_level
-        self.blackpoint_label = QtWidgets.QLabel("")
-        layout.addWidget(self.blackpoint_label)
-        self.blackpoint_slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self.blackpoint_slider.setRange(black_level.range[0], black_level.range[1])
-        self.blackpoint_slider.setValue(black_level.default_value)
-        self.blackpoint_changed()
-        self.blackpoint_slider.valueChanged.connect(lambda: self.blackpoint_changed(callback))
-        layout.addWidget(self.blackpoint_slider)
-
-    def blackpoint_reset(self):
-        self.blackpoint_slider.setValue(self.params.black_level.default_value)
-
-    def blackpoint_changed(self, callback=None):
-        black_level = self.params.black_level
-        black_level.value = int(self.blackpoint_slider.value())
-        self.blackpoint_label.setText("Black {}".format(black_level.value))
-        if callback is not None:
-            callback()
+        self.bl_gui = NumericParameterGui("Black", self.params.black_level, callback, layout, self.name)
+        self.bl_gui.set_event_recorder(self.event_recorder)
 
     def add_whitepoint(self, layout, callback):
-        """
-        White point adjustment
-        """
-        white_level = self.params.white_level
-        self.whitepoint_label = QtWidgets.QLabel("")
-        layout.addWidget(self.whitepoint_label)
-        self.whitepoint_slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self.whitepoint_slider.setRange(white_level.range[0], white_level.range[1])
-        self.whitepoint_slider.setValue(white_level.default_value)
-        self.whitepoint_changed()
-        self.whitepoint_slider.valueChanged.connect(lambda: self.whitepoint_changed(callback))
-        layout.addWidget(self.whitepoint_slider)
-
-    def whitepoint_reset(self):
-        self.whitepoint_slider.setValue(self.params.white_level.default_value)
-
-    def whitepoint_changed(self, callback=None):
-        white_level = self.params.white_level
-        white_level.value = int(self.whitepoint_slider.value())
-        self.whitepoint_label.setText("White {}".format(white_level.value))
-        if callback is not None:
-            callback()
+        self.wl_gui = NumericParameterGui("White", self.params.white_level, callback, layout, self.name)
+        self.wl_gui.set_event_recorder(self.event_recorder)
 
     def add_gamma(self, layout, callback):
-        """
-        Gamma adjustment
-        :param layout:
-        :param callback:
-        :return:
-        """
-        gamma = self.params.gamma
-        self.gamma_label = QtWidgets.QLabel("Gamma 1.00")
-        layout.addWidget(self.gamma_label)
-        self.gamma_slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self.gamma_slider.setRange(gamma.range[0], gamma.range[1])
-        self.gamma_slider.setValue(gamma.default_value)
-        self.gamma_slider.valueChanged.connect(lambda: self.gamma_changed(callback))
-        layout.addWidget(self.gamma_slider)
+        self.gamma_gui = NumericParameterGui("Gamma", self.params.gamma, callback, layout, self.name)
+        self.gamma_gui.set_event_recorder(self.event_recorder)
 
-    def gamma_reset(self):
-        self.gamma_slider.setValue(self.params.gamma.default_value)
+    def add_g_r(self, layout, callback):
+        self.g_r_gui = NumericParameterGui("G/R", self.params.g_r, callback, layout, self.name)
+        self.g_r_gui.set_event_recorder(self.event_recorder)
 
-    def gamma_changed(self, callback):
-        gamma = self.params.gamma
-        gamma.value = int(self.gamma_slider.value())
-        self.gamma_label.setText("Gamma  {:0.2f}".format(gamma.get_float()))
-        callback()
+    def add_g_b(self, layout, callback):
+        self.g_b_gui = NumericParameterGui("G/B", self.params.g_b, callback, layout, self.name)
+        self.g_b_gui.set_event_recorder(self.event_recorder)
+
+    def register_event_player(self, event_player):
+        for v in vars(self):
+            if 'gui' in v:
+                self.__dict__[v].register_event_player(event_player)
+
+    def reset_all(self):
+        for v in vars(self):
+            if 'gui' in v:
+                self.__dict__[v].reset()
+
