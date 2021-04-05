c947daf57faa898968a816bdb4635a81e3717221
karl krissian <karl.krissian@gmail.com>
2021-04-06 00:51:57 +0200

    Add Multimedia to default qt5 imports

--- utils/qt_imports.py
+++ utils/qt_imports.py
@@ -5,12 +5,12 @@
 import Qt
 
 if Qt.__binding__ == "PySide2":
-    from PySide2 import QtGui, QtWidgets, QtCore
+    from PySide2 import QtGui, QtWidgets, QtCore, QtMultimedia, QtMultimediaWidgets
     from PySide2.QtOpenGL import QGLWidget
     from PySide2.QtWidgets import QOpenGLWidget as QOpenGLWidget
     from PySide2.QtCore import Signal, Slot
 else:
-    from PyQt5 import QtGui, QtWidgets, QtCore
+    from PyQt5 import QtGui, QtWidgets, QtCore, QtMultimedia, QtMultimediaWidgets
     from PyQt5.QtOpenGL import QGLWidget
     from PyQt5.QtWidgets import QOpenGLWidget as QOpenGLWidget
     from PyQt5.QtCore import pyqtSignal as Signal, pyqtSlot as Slot
