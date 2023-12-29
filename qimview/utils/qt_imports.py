35ce49b13ed310ca4353e572e96bcbb28a5bfa32
karl krissian <karl.krissian@gmail.com>
2023-12-29 17:43:43 +0100

    Add import of QOpenGLTexture

--- qimview/utils/qt_imports.py
+++ qimview/utils/qt_imports.py
@@ -1,6 +1,7 @@
 
 from PySide6 import QtGui, QtWidgets, QtCore, QtMultimedia, QtMultimediaWidgets
-from PySide6.QtOpenGLWidgets import QOpenGLWidget as QOpenGLWidget
+from PySide6.QtOpenGLWidgets import QOpenGLWidget
+from PySide6.QtOpenGL import QOpenGLTexture
 from PySide6.QtCore import Signal, Slot
 
 QLabel = QtWidgets.QLabel
