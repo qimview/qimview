bbc911490cfff706891514ea3336fbe28acf207e
karl krissian <karl.krissian@gmail.com>
2024-01-01 17:46:00 +0100

    Improve buttons and video slider + clean-up code based on scheduler

--- qimview/video_player/video_player_av.py
+++ qimview/video_player/video_player_av.py
@@ -11,7 +11,10 @@ from qimview.image_viewers.qt_image_viewer import QTImageViewer
 from qimview.image_viewers.gl_image_viewer_shaders import GLImageViewerShaders
 from qimview.utils.viewer_image import ViewerImage, ImageFormat
 from qimview.utils.thread_pool import ThreadPool
-from qimview.video_player.video_scheduler import VideoScheduler
+from qimview.video_player.video_scheduler     import VideoScheduler
+from qimview.parameters.numeric_parameter     import NumericParameter
+from qimview.parameters.numeric_parameter_gui import NumericParameterGui
+
 
 class AverageTime:
     def __init__(self):
@@ -95,25 +98,41 @@ class VideoPlayerAV(QtWidgets.QWidget):
         self.widget._show_text = False
         self.setGeometry(0, 0, self.widget.width(), self.widget.height())
         hor_layout = QtWidgets.QHBoxLayout()
-        button2 = QtWidgets.QPushButton("AV")
-        button3 = QtWidgets.QPushButton("Thread Test")
-        button_pauseplay = QtWidgets.QPushButton(">||")
-        hor_layout.addWidget(button2)
-        hor_layout.addWidget(button3)
-        hor_layout.addWidget(button_pauseplay)
+        button_start = QtWidgets.QPushButton("Start")
+        button_pause = QtWidgets.QPushButton()
+        icon_pause = self.style().standardIcon(QtWidgets.QStyle.StandardPixmap.SP_MediaPause)
+        button_pause.setIcon(icon_pause)
+        button_play  = QtWidgets.QPushButton()
+        icon_play = self.style().standardIcon(QtWidgets.QStyle.StandardPixmap.SP_MediaPlay)
+        button_play.setIcon(icon_play)
+        hor_layout.addWidget(button_start)
+        hor_layout.addWidget(button_pause)
+        hor_layout.addWidget(button_play)
         vertical_layout.addWidget(self.widget)
         vertical_layout.addLayout(hor_layout)
+
+        # create slider
+        # TODO: move this variable to __init__()
+        self.play_position = NumericParameter()
+        self.play_position.float_scale = 1000
+        self.play_position_gui = NumericParameterGui(name="sec:", param=self.play_position,
+                                                     callback=self.set_play_position)
+        self.play_position_gui.decimals = 3
+        self.play_position_gui.create(moved_callback=True)
+        self.play_position_gui.add_to_layout(hor_layout)
+
         # is show required here?
         self.show()
         self._im = None
         self._pause_time : float = 0
         self._pause = False
-        button2         .clicked.connect(self.start_video_av)
-        button3         .clicked.connect(self.start_decode_with_thread)
-        button_pauseplay.clicked.connect(self.pause_play)
-        self._container = None
-        self.filename   : str = ''
+        button_start.clicked.connect(self.start_decode)
+        button_pause.clicked.connect(self.pause)
+        button_play .clicked.connect(self.play)
+        self._container : container.InputContainer | None = None
+        self._video_stream = None
 
+        self._scheduler : VideoScheduler = VideoScheduler()
         self._start_clock_time : float = 0
         self._start_video_time : float = 0
         self._skipped : int = 0
@@ -133,20 +152,34 @@ class VideoPlayerAV(QtWidgets.QWidget):
     def set_video(self, filename):
         self.filename = filename
 
-    def pause_play(self):
-        self._pause = not self._pause
-        print(f"self._pause = {self._pause}")
-        if self._pause and self._timer:
-            self._timer.stop()
-            self._pause_time = float(self._frame.pts * self._frame.time_base)
-        else:
-            self.set_time(self._pause_time, self._container)
-            self._start_video_time = self._pause_time
-            self._start_clock_time = time.perf_counter()
-            self._timer.start()
+    def set_pause(self):
+        self._pause = True
+        self._pause_time = float(self._frame.pts * self._frame.time_base)
+    
+    def set_play(self):
+        self.set_time(self._pause_time)
+        self._start_video_time = self._pause_time
+        self._start_clock_time = time.perf_counter()
+        self._pause = False
 
-        if not self._pause:
-            self.start_video_av(self._pause_time)
+    def pause(self):
+        self._scheduler.pause()
+    
+    def play(self):
+        self._scheduler.play()
+
+    def set_play_position(self):
+        self._scheduler.pause()
+        print(f"self.play_position {self.play_position.float}")
+        self.set_time(self.play_position.float)
+        self._pause_time = self.play_position.float
+        self.display_frame(self._frame)
+
+    def update_position(self, precision=0.02):
+        current_time = float(self._frame.pts * self._frame.time_base)
+        if abs(self.play_position.float-current_time)>precision:
+            self.play_position.float = current_time
+            self.play_position_gui.updateGui()
 
     def set_image(self, np_array, im_name, force_new=False):
         if self._im is None or force_new:
@@ -184,21 +217,29 @@ class VideoPlayerAV(QtWidgets.QWidget):
     def set_synchronize(self, viewer):
         self.synchronize_viewer = viewer
 
-    def set_time(self, time_pos : float, container):
+    def set_time(self, time_pos : float):
         """ set time position in seconds """
-        framerate = container.streams.video[0].average_rate # get the frame rate
-        frame_num = int(time_pos*framerate)
-        time_base = container.streams.video[0].time_base # get the time base
-        container.seek(int(time_pos*1000000), whence='time', backward=True)  # seek to that nearest timestamp
-        frame = next(container.decode(video=0)) # get the next available frame
-        sec_frame = int(frame.pts * time_base * framerate)  # get the proper key frame number of that timestamp
+        if self._container is None:
+            print("Video not initialized")
+        else:
+            framerate = self._video_stream.average_rate # get the frame rate
+            frame_num = int(time_pos*framerate+0.5)
+            time_base = float(self._video_stream.time_base) # get the time base
+            # seek to that nearest timestamp
+            self._container.seek(int(time_pos*1000000), whence='time', backward=True)
+            self._frame_generator = self._container.decode(video=0)
+            # get the next available frame
+            frame = next(self._frame_generator)
+            # get the proper key frame number of that timestamp
+            sec_frame = int(frame.pts * float(time_base) * framerate)
 
-        print(f"got frame at {float(frame.pts * time_base)}, expected {time_pos}")
-        print(f"missing {int((time_pos-sec_frame)/float(1000000*time_base))} time_base {time_base}")
-        for _ in range(sec_frame, frame_num):
-           frame = next(container.decode(video=0))
-        sec_frame = int(frame.pts * time_base * framerate)
-        print(f"-> got frame at {float(frame.pts * time_base)}, expected {time_pos}")
+            print(f"got frame at {float(frame.pts * time_base)}, expected {time_pos}")
+            # print(f"missing {int((time_pos-sec_frame)/float(1000000*time_base))} ")
+            for _ in range(sec_frame, frame_num):
+                frame = next(self._frame_generator)
+            sec_frame = int(frame.pts * time_base * framerate)
+            print(f"-> got frame at {float(frame.pts * time_base)}, expected {time_pos}")
+            self._frame = frame
 
     def display_frame_RGB(self, frame: av.VideoFrame):
         """ Convert YUV to RGB and display RGB frame """
@@ -257,7 +298,7 @@ class VideoPlayerAV(QtWidgets.QWidget):
         """ Display YUV frame, for OpenGL with shaders """
         # a = frame.to_ndarray(format='rgb24')
         # to start with keep all pixels, then crop only at the end
-        print("display YUV")
+        # print("display YUV")
         y,u,v = VideoPlayerAV.to_yuv(frame)
         # print(f"y min {np.min(y)} y max {np.max(y)}")
         self.set_image_YUV420(y,u,v, f"frame_{frame.pts}")
@@ -272,92 +313,40 @@ class VideoPlayerAV(QtWidgets.QWidget):
         else:
             self.display_frame_RGB(frame)
 
-    def sync_frame(self, frame):
-        """ Display frame at the expected time, either skip frames or wait if time does not match """
-        self.frame_number += 1
-        time_base = float(frame.time_base)
-        frame_time : float = float(frame.pts * time_base) - self._start_video_time
-        time_spent : float = time.perf_counter() - self._start_clock_time
-        # print(f"frame_time {frame_time} time_spent {time_spent}")
-        if self._pause:
-            self._pause_time = float(frame.pts * time_base)
-            print(f"paused")
-            return False
-        if time_spent>frame_time and frame.pts%(3*self._ticks_per_frame) != 0:
-
-            while time_spent>frame_time and frame.pts%(3*self._ticks_per_frame) != 0:
-                frame = next(self.frame_generator)
-                # print(frame.pts, end=' - ')
-                self._skipped +=1
-                frame_time = frame.pts * time_base - self._start_video_time
-                time_spent = time.perf_counter() - self._start_clock_time
-        if time_spent<frame_time:
-            # wait a little bit
-            while(time_spent<frame_time):
-                time_spent = time.perf_counter() - self._start_clock_time
-                # loop = QtCore.QEventLoop()
-                # QtCore.QTimer.singleShot(4, loop.exit)
-                # loop.exec()
-                QtWidgets.QApplication.processEvents(QtCore.QEventLoop.ProcessEventsFlag.AllEvents, 4)
-
-        self.display_frame(frame)
-
-        ## Allow 1 ms
-        if frame.pts%(6*self._ticks_per_frame)==0:
-            loop = QtCore.QEventLoop()
-            QtCore.QTimer.singleShot(2, loop.exit)
-            loop.exec()
-            # QtWidgets.QApplication.processEvents(QtCore.QEventLoop.ProcessEventsFlag.AllEvents, 1)
-        if frame.pts%(100*self._ticks_per_frame) == 0:
-            print(f"frame {int(frame.pts/self._ticks_per_frame+0.5)}, skipped {self._skipped}")
-            print(f"{float(frame.pts * time_base):0.2f} vs "
-                    f"time spent {time.perf_counter() - self._start_clock_time + self._start_video_time:0.2f}")
-        return True
-
     def init_video_av(self):
         """ Initialize the container and frame generator """
-        if self._container is None:
-            print(f"filename = {self.filename}")
-            self._container : container.InputContainer = av.open(self.filename)
-            self._container.streams.video[0].thread_type = "AUTO"
-            self.video_stream = self._container.streams.video[0]
-            framerate = float(self.video_stream.average_rate) # get the frame rate
-            print(f"framerate {framerate}")
-            # duration = probe['format']['duration']
-            # fps = eval(probe['streams'][0]['r_frame_rate'])
-            # print(f"fps={fps}")
-            # width = int(video_stream['width'])
-            # height = int(video_stream['height'])
+        if self._scheduler._timer.isActive():
+            self._scheduler.pause()
+        print(f"filename = {self.filename}")
+        self._container : container.InputContainer = av.open(self.filename)
+        self._container.streams.video[0].thread_type = "AUTO"
+        self._video_stream = self._container.streams.video[0]
+        framerate = float(self._video_stream.average_rate) # get the frame rate
+        print(f"framerate {framerate}")
 
-            fps = self.video_stream.base_rate
-            time_base = self.video_stream.time_base
-            self._ticks_per_frame = int((1/fps) / time_base)
-            print(f"ticks_per_frame {self._ticks_per_frame}")
+        duration = float(self._video_stream.duration * self._video_stream.time_base)
+        print(f"duration = {duration} seconds")
+        self.play_position.range = [0, int(duration*self.play_position.float_scale)]
+        self.play_position_gui.setRange(0, int(duration*self.play_position.float_scale))
+        self.play_position_gui.changed()
 
-            self.frame_generator : Generator = self._container.decode(video=0)
-            self.frame_number = -1
-            self.yuv_array = np.empty((1), dtype=np.uint8)
+        # fps = eval(probe['streams'][0]['r_frame_rate'])
+        # print(f"fps={fps}")
+        # width = int(video_stream['width'])
+        # height = int(video_stream['height'])
 
-    def start_video_av(self, start_time: float =0):
-        if self._container is None: self.init_video_av()
-        self.set_time(start_time, self._container)
-        self._start_video_time = start_time
+        fps = self._video_stream.base_rate
+        time_base = self._video_stream.time_base
+        self._ticks_per_frame = int((1/fps) / time_base)
+        print(f"ticks_per_frame {self._ticks_per_frame}")
 
-        # Starting time in seconds
-        self._start_clock_time = time.perf_counter()
-        self._skipped = 0
-        # expect format to be YUVJ420P, so values in 0-255
-        n = 0
-        for frame in self._container.decode(video=0):
-            if n==0:
-                print(f"frame format '{frame.format}'")
-            ok = self.sync_frame(frame)
-            n += 1
-            if not ok: break
+        self._frame_generator : Generator = self._container.decode(video=0)
+        self.frame_number = -1
+        self.yuv_array = np.empty((1), dtype=np.uint8)
 
-    def get_next_frame(self):
+    def get_next_frame(self, verbose=False):
         t = time.perf_counter()
-        self._frame = next(self.frame_generator)
+        self._frame = next(self._frame_generator)
         self.frame_number += 1
         d = time.perf_counter()-t
         s = 'gen '
@@ -365,89 +354,15 @@ class VideoPlayerAV(QtWidgets.QWidget):
         s += f'{d:0.3f} ' if d>=0.001 else ''
         s += f'{self._frame.pict_type}'
         s += f' {self._frame.pts}'
-        if s != 'gen P': print(s)
-        else: print(s)
+        if verbose:
+            if s != 'gen P': print(s)
+            else: print(s)
 
-    def check_next_frame(self, progress_callback=None):
-        """ Check if we need to get a new frame based on the time spent """
-        # print('==')
-        # st = time.perf_counter()
-        # if self._frame is None: self.get_next_frame()
-        assert self._frame is not None, "No frame available"
-        time_base = float(self._frame.time_base)
-        if self._pause:
-            self._pause_time = float(self._frame.pts * time_base)
-            print(f"paused")
-            return False
-        next_frame_time : float = float((self._frame.pts+self._ticks_per_frame) * time_base) - self._start_video_time
-        time_spent : float = time.perf_counter() - self._start_clock_time
-        # print(f"{next_frame_time} {time_spent}")
-
-        if time_spent>next_frame_time:
-            iter = 0
-            while time_spent>next_frame_time and iter<10:
-                if iter>0:
-                    self._skipped +=1
-                    print(f" skipped {self._skipped},")
-                self.get_next_frame()
-                next_frame_time = float((self._frame.pts+self._ticks_per_frame) * time_base) - self._start_video_time
-                time_spent = time.perf_counter() - self._start_clock_time
-                iter +=1
-
-    def _display_current_frame(self):
-        print(f"*** {self._name} {time.perf_counter():0.4f}", end=' --')
-        if self._frame.pts != self._displayed_pts:
-            frame_time : float = float(self._frame.pts * float(self._frame.time_base)) - self._start_video_time
-            time_spent : float = time.perf_counter() - self._start_clock_time
-            if abs(time_spent-frame_time)>=self._timer.interval()*1000:
-                print(f" frame {frame_time:0.3f} at time {time_spent:0.3f}")
-            # print(f"{self.widget.context()}")
-            self.display_frame(self._frame)
-            self._displayed_pts = self._frame.pts
-        print(f" done {self._name} {time.perf_counter():0.4f}")
-
-    def _timer_cmds(self):
-        # print(f" _timer_cmds() {self._timer_counter} - ")
-        self._timer_counter += 1
-        if self._timer_counter == 1000:
-            self._timer.stop()
-            print(f" skipped = {self._skipped}")
-
-        self._thread_pool.start_worker()
-        self._thread_pool.waitForDone()
-        # print(f"time spent {(time.perf_counter()-self._start_clock_time)*1000:0.2f} ms")
-
-    def start_decode_with_thread(self):
-        # self.set_time(start_time, self._container)
-        # self._start_video_time = start_time
-
-        if self._container is None:
-            print("init")
-            self.init_video_av()
-            # get the first frame which is slower
-            self.get_next_frame()
-            self._display_current_frame()
-
-        # Starting time in seconds
-        if self._start_clock_time == 0:
-            self._start_clock_time = time.perf_counter()
-            self._skipped = 0
-
-        # have a timer within the thread, probably better?
-        # expect format to be YUVJ420P, so values in 0-255
-        self._displayed_pts : int = -1
-        self._timer : QtCore.QTimer = QtCore.QTimer()
-        self._timer_counter = 0
-
-        self._thread_pool : ThreadPool = ThreadPool()
-        self._thread_pool.set_worker(self.check_next_frame)
-        self._thread_pool.set_autodelete(False)
-        self._thread_pool.set_worker_callbacks(finished_cb=self._display_current_frame)
-
-        # Set a 5 ms counter
-        self._timer.timeout.connect(self._timer_cmds)
-        self._timer.start(5)
-
+    def start_decode(self):
+        if self._scheduler._timer.isActive():
+            self._scheduler.pause()
+        self._scheduler.set_players([self])
+        self._scheduler.start_decode()
 
 def main():
     import argparse
@@ -491,8 +406,8 @@ def main():
     video_layout.addWidget(player2, 1)
     main_layout.addLayout(video_layout)
 
-    button_pauseplay = QtWidgets.QPushButton(">||")
-    main_layout.addWidget(button_pauseplay)
+    # button_pauseplay = QtWidgets.QPushButton(">||")
+    # main_layout.addWidget(button_pauseplay)
     # button_pauseplay.clicked.connect(lambda: pause_play(player1, player2))
 
     sch = VideoScheduler(10)
