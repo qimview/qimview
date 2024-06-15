ee07cbaa2e77fae1bbbb3299749f08c6fa39b0dd
karl krissian <karl.krissian@gmail.com>
2024-06-15 15:01:02 +0200

    Update imports and class name

--- qimview/video_player/test_videoplayer.py
+++ qimview/video_player/test_videoplayer.py
@@ -1,10 +1,10 @@
 
 from utils.qt_imports import QtWidgets
 import sys
-from video_player.video_player import VideoPlayer
+from video_player.qt_video_player import QtVideoPlayer
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    window = VideoPlayer(None, open_button=True)
+    window = QtVideoPlayerVideoPlayer(None, open_button=True)
     sys.exit(app.exec_())
