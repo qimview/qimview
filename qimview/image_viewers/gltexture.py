1cde7ba7842bdc48784ed7cfc68dff39f27e7e3b
karl krissian <karl.krissian@gmail.com>
2024-11-06 14:14:57 +0100

    WIP: Support for QOpenGLTexture in GLTexture

--- qimview/image_viewers/gltexture.py
+++ qimview/image_viewers/gltexture.py
@@ -4,7 +4,7 @@ import numpy as np
 import OpenGL
 OpenGL.ERROR_ON_COPY = True
 import OpenGL.GL as gl
-from qimview.utils.qt_imports   import QtGui, QOpenGLTexture
+from qimview.utils.qt_imports   import QtGui, QOpenGLTexture, QImage
 from qimview.utils.viewer_image import ImageFormat, ViewerImage
 import time
 
@@ -30,6 +30,21 @@ class GLTexture:
             'float32': gl.GL_FLOAT,
             'float64': gl.GL_DOUBLE
         }
+        self._qt_gl_types = {
+            'int8'   : QOpenGLTexture.PixelType.Int8,
+            'uint8'  : QOpenGLTexture.PixelType.UInt8,
+            'int16'  : QOpenGLTexture.PixelType.Int16,
+            'uint16' : QOpenGLTexture.PixelType.UInt16,
+            'int32'  : QOpenGLTexture.PixelType.Int32,
+            'uint32' : QOpenGLTexture.PixelType.UInt32,
+            'float32': QOpenGLTexture.PixelType.Float32,
+        }
+        self._qimage_types = {
+            'int8'   : QImage.Format.Format_Grayscale8,
+            'uint8'  : QImage.Format.Format_Grayscale8,
+            'int16'  : QImage.Format.Format_Grayscale16,
+            'uint16' : QImage.Format.Format_Grayscale16,
+        }
         self._channels2format = {
                 ImageFormat.CH_RGB : gl.GL_RGB,
                 ImageFormat.CH_BGR : gl.GL_BGR,
@@ -38,6 +53,15 @@ class GLTexture:
                 ImageFormat.CH_GRBG: gl.GL_RGBA,
                 ImageFormat.CH_GBRG: gl.GL_RGBA,
                 ImageFormat.CH_BGGR: gl.GL_RGBA
+            }
+        self._qt_channels2format = {
+                ImageFormat.CH_RGB : QOpenGLTexture.PixelFormat.RGB,
+                ImageFormat.CH_BGR : QOpenGLTexture.PixelFormat.BGR,
+                ImageFormat.CH_Y   : QOpenGLTexture.PixelFormat.Red, # not sure about this one
+                ImageFormat.CH_RGGB: QOpenGLTexture.PixelFormat.RGBA, # we save 4 component data
+                ImageFormat.CH_GRBG: QOpenGLTexture.PixelFormat.RGBA,
+                ImageFormat.CH_GBRG: QOpenGLTexture.PixelFormat.RGBA,
+                ImageFormat.CH_BGGR: QOpenGLTexture.PixelFormat.RGBA
             }
         self.width  : int = 0
         self.height : int = 0
@@ -137,6 +161,56 @@ class GLTexture:
         if self.counter[name] == 30:
             print(f" {name}: textSubImage() av {self.counter[name]} = {(self.timing[name]/self.counter[name])*1000:0.1f} ms")
             self.timing[name], self.counter[name] = 0, 0
+
+    def create_texture_qt_gl(self, image: ViewerImage):
+        """ Creates a QOpenGLTexture from a ViewerImage
+
+        Args:
+            image (ViewerImage): _description_
+        """
+        height, width = image.data.shape[:2]
+        qt_gl_type = self._qt_gl_types[image.data.dtype.name]
+        if image.channels == ImageFormat.CH_YUV420:
+            LUM = QOpenGLTexture.PixelFormat.Luminance
+            match image.data.dtype:
+                case np.uint8:  format = QOpenGLTexture.PixelFormat.Red_Integer
+                case np.uint16: format = QOpenGLTexture.PixelFormat.Red_Integer
+                case _:
+                    print(f"create_texture_gl(): Image format not supported {image.data.dtype}")
+                    format = QOpenGLTexture.PixelFormat.Red
+            self.interlaced_uv = image.uv is not None
+            if self.interlaced_uv:
+                RG = gl.GL_RG
+                match image.data.dtype:
+                    case np.uint8:  format_uv = QOpenGLTexture.PixelFormat.RG_Integer
+                    case np.uint16: format_uv = QOpenGLTexture.PixelFormat.RG_Integer
+                    case _:
+                        print(f"create_texture_gl(): Image format not supported {image.data.dtype}")
+                        format_uv = QOpenGLTexture.PixelFormat.RG
+            w, h = width, height
+            w2, h2 = int(w/2), int(h/2)
+            if (self.width,self.height) != (width,height) or self.textureY is None:
+                self.textureY = self.new_texture(self.textureY)
+                # Create QImage
+                image = QtGui.QImage(image.data, w, h, )
+                self.set_default_parameters(self.textureY)
+                self._gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, format, w, h, 0, LUM, gl_type, image.data)
+                self.width, self.height = width, height
+                if self.interlaced_uv:
+                    # UV
+                    self.textureUV = self.new_texture(self.textureUV)
+                    self.set_default_parameters(self.textureUV)
+                    self._gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, format_uv, w2, h2, 0, RG, gl_type, image.uv)
+                else:
+                    # U
+                    self.textureU = self.new_texture(self.textureU)
+                    self.set_default_parameters(self.textureU)
+                    self._gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, format, w2, h2, 0, LUM, gl_type, image.u)
+                    # V
+                    self.textureV = self.new_texture(self.textureY)
+                    self.set_default_parameters(self.textureV)
+                    self._gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, format, w2, h2, 0,LUM, gl_type, image.v)
+
 
     def create_texture_gl(self, image: ViewerImage):
         """ Create an OpenGL texture from a ViewerImage using OpenGL functions """
